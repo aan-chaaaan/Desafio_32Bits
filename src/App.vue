d48140d05ce921cfedfae32b4a291ff2660e5e3b
Angela <angela.valdebenito.v@gmail.com>
2021-09-06 19:53:36 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ul>
-      <li><router-link :to="{name: 'inicio'}">Inicio</router-link></li>
-    </ul>
     <transition name="vista">
       <router-view></router-view>
     </transition>
@@ -11,17 +7,8 @@
 </template>
 
 <script>
-
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
-
-.vista-enter-active, .vista-leave-active {
-    transition: opacity .1s;
-}
-.vista-enter, .vista-leave-to{
-    opacity: 0;
-}
 </style>
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>
-        Acá va tu contenido
-    </div>
+  <div id="App">
+    <h1>31bits</h1>
+    <h2>Juegos de pc y consolas</h2>
+    <ul>
+      <li><router-link to="/">inicio</router-link></li>
+      <li><router-link to="/busquedas">Busquedas</router-link></li>
+      <li><router-link to="/ventas">Ventas</router-link></li>
+      <li><router-link to="/total">Total</router-link></li>
+    </ul>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 export default {
-}
+  data() {
+    return {
+      searchCode: "",
+    };
+  },
+  computed: {
+    ...mapState({}),
+  },
+};
 </script>
