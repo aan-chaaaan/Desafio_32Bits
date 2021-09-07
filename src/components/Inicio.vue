b60d545e3aee18a125b3af3a330386bf9434c4f9
angela <angela.valdebenito.v@gmail.com>
2021-09-06 21:05:50 -0300

    commit

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -4,7 +4,7 @@
     <h2>Juegos de pc y consolas</h2>
     <ul>
       <li><router-link to="/">inicio</router-link></li>
-      <li><router-link to="/busquedas">Busquedass</router-link></li>
+      <li><router-link to="/busquedas">Busquedas</router-link></li>
       <li><router-link to="/ventas">Ventas</router-link></li>
       <li><router-link to="/total">Total</router-link></li>
     </ul>
